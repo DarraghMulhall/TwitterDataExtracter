3d5e819e6024a2de62b7ab3ffa5d7546914c934a
DarraghMulhall <darraghmulhall1@gmail.com>
2018-02-28 14:23:32 +0000

    Moving to laptop

--- index.js
+++ index.js
@@ -1,21 +1,4 @@
-
-// tweets (replies + posts) - linguistic analysis
-// num replies
-// num posts
-
-// listCount
-
-//followers_count
-//friends_count (following)
-
-//description - linguistic analysis
-
-//favourites_count (number user has liked)
-
-
-
-
- var CONFIG = require('./config.json');
+var CONFIG = require('./config.json');
 
 var Twitter = require('twitter');
 var https = require('https');
@@ -23,6 +6,79 @@ var http = require('http')
 var mysql = require('mysql');
 
 
+var username = ''
+var scores = {}
+
+var fs = require('fs')
+
+
+
+http.createServer(function (req, res) {
+        res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.write('Hello World!');
+        console.log(req.method)
+        if (req.method == 'POST') {
+                console.log("POST it is");
+//      process.exit()
+        var body = '';
+        req.on('data', function (data) {
+            body += data;
+            console.log("Partial body: " + body);
+        });
+        req.on('end', function () {
+           var userScores = JSON.parse(body);
+        res.end()
+           scores = userScores["scores"]
+           main()
+        });
+    }
+  res.end();
+}).listen(3000);
+
+
+
+function checkAndHandleForExistingUser(){
+        var userExists = false
+        var con = mysql.createConnection({
+        host: "twitter-database.ci3cppc9rrzd.eu-west-2.rds.amazonaws.com",
+        user: "DarraghMulhall",
+        password: "daramul96",
+        database: "TwitterUserData"
+      });
+
+    con.connect(function(err) {
+        if (err) throw err;
+        console.log("Connected!");
+
+        //if user already exists in database, delete their scores entry for new one and insert current given scores
+        var userCheckSql = "SELECT * FROM user_data WHERE username = '"+username+"'"
+        con.query(userCheckSql, function (err, result) {
+            if (err) throw err;
+            if(result.length!=0){
+                userExists = true
+                console.log("shud delete")
+                var deleteUserScores = "DELETE FROM big_five_scores WHERE username = '"+username+"'"
+
+                con.query(deleteUserScores, function (err, result) {
+                    if (err) throw err;
+                 //   con.end()
+                });
+                }
+                var insertScoresSql = "INSERT INTO big_five_scores (username, ext, neu, agr, con, opn) VALUES "
+                        +"('"+username+"', "+scores["ext"]+", "+scores["neu"]+", "+scores["agr"]+", "+scores["con"]+", "+scores["opn"]+")"
+
+                 con.query(insertScoresSql, function (err, result) {
+                    if (err) throw err;
+                 con.end()
+                });
+
+        });
+        });
+        return userExists
+}
 
 
 function getTwitterInfo(){
@@ -33,10 +89,10 @@ function getTwitterInfo(){
         'grant_type': 'client_credentials'
     }, function (e, access_token) {
         console.log(access_token); //string that we can use to authenticate request
-        
+
         var options = {
             hostname: 'api.twitter.com',
-            path: '/1.1/statuses/user_timeline.json?screen_name=notskend&count=200&tweet_mode=extended&page=1',
+            path: '/1.1/statuses/user_timeline.json?screen_name=+'+username+'&count=200&tweet_mode=extended&page=1',
             headers: {
                 Authorization: 'Bearer ' + access_token
             }
@@ -46,24 +102,28 @@ function getTwitterInfo(){
         https.get(options, function (result) {
             var buffer = '';
             result.setEncoding('utf8');
-            var text_arr = []
             result.on('data', function (data) {
                 buffer += data;
             });
-            result.on('end', async function () {
-              //console.log(text_arr)
+            result.on('end', function () {
                 var tweets = JSON.parse(buffer);
-                //console.log(tweets.length)
-                for(var j=0; j<tweets.length; j++){
-                    //console.log(tweets[j]['full_text'] + " " + tweets[j]['created_at'] + "\n")
+
+                if(tweets.length == null){
+                    console.log("Invalid username")
                 }
-                if(tweets.length > 0)
+                else {
+                    if(tweets.length > 0)
                     total_statuses = tweets[0]['user']['statuses_count']
 
-                var iterationsLeft =(total_statuses)/200 + 1
-                //console.log(iterationsLeft)
-                var iteration = 2
-                getPageOfTweets(tweets, iteration, iterationsLeft, access_token)
+                    var iterations =Math.ceil((total_statuses)/200)
+
+                    if(iterations > 16){
+                        iterations = 16
+                    }
+                    var iteration = 2
+                    getPageOfTweets(tweets, iteration, iterations, access_token)
+                }
+
             });
         });
     });
@@ -150,7 +210,27 @@ function mentionsPerTweet(tweets){
     return total_mentions/tweets.length
 }
 
+function calcAveragePostLength(tweets, posts_count){
+    var average_length = 0.0
+    for(var i=0; i<tweets.length; i++){
+        if(tweets[i]["full_text"].charAt(0) != '@' && tweets[i]["full_text"].substring(0, 2) != 'RT'){
+            average_length += tweets[i]["full_text"].length
+        }
+    }
 
+    return average_length/posts_count
+}
+
+
+function calcAverageReplyLength(tweets, replies_count){
+    var average_length = 0.0
+    for(var i=0; i<tweets.length; i++){
+        if(tweets[i]["full_text"].charAt(0) == '@'){
+            average_length += tweets[i]["full_text"].length
+        }
+    }
+    return average_length/replies_count
+}
 
 
 
@@ -189,48 +269,52 @@ function insertDataToDatabase(tweets){
 
     var wordsPerTweet = posts_obj["posts"].split(" ").length/posts_count
 
-    var wordsPerReplies = replies_obj["replies"].split(" ").length/replies_count
+    var wordsPerReply = replies_obj["replies"].split(" ").length/replies_count
 
     var averageMentionsPerTweet = mentionsPerTweet(tweets)
 
     var linguistic_data = posts + " " + replies + " " + description
-    
-    // var con = mysql.createConnection({
-    //     host: "localhost",
-    //     user: "yourusername",
-    //     password: "yourpassword",
-    //     database: "mydb"
-    //   });
 
-    // con.connect(function(err) {
-    //     if (err) throw err;
-    //     console.log("Connected!");
-        var sql = "INSERT INTO twitter_users (tweets, total_status_count, posts_count, replies_count, retweets_count, listed_count"+
-        ",favourites_count, following_count, followers_count, desc_length, tweets_retweeted_count, tweets_favourited_count, hashtags_per_tweet"+
-        ", urlsPerTweet, mentions_per_tweet, words_per_tweet, words_per_replies) VALUES ("+linguistic_data+", "+total_statuses+", "+posts_count+", "+replies_count+", "+retweets_count+
-        ", "+listed_count+", "+favourites_count+", "+following_count+", "+followers_count+", "+description_length+", "+retweets_count+", "+favourite_count+", "+averageHashtagNumPerTweet+
-        ", "+averageUrlNumPerTweet+", "+averageMentionsPerTweet+", "+wordsPerTweet+", "+wordsPerReplies+")";
-        console.log(sql)
-        // con.query(sql, function (err, result) {
-        //     if (err) throw err;
-        //     console.log("1 record inserted");
-        // });
-    //});
+    var averagePostLength = calcAveragePostLength(tweets, posts_count)
+    var averageReplyLength = calcAverageReplyLength(tweets, replies_count)
 
+    var con = mysql.createConnection({
+        host: "twitter-database.ci3cppc9rrzd.eu-west-2.rds.amazonaws.com",
+        user: "DarraghMulhall",
+        password: "daramul96",
+        database: "TwitterUserData"
+      });
 
+    con.connect(function(err) {
+        if (err) throw err;
+        console.log("Connected!");
+
+                        var user_data_sql = "INSERT INTO user_data (username, total_status_count, posts_count, replies_count, retweets_count, listed_count"+
+                ", favourites_count, following_count, followers_count, desc_length, tweets_retweeted_count, tweets_favourited_count, hashtags_per_tweet"+
+                ", urls_per_tweet, mentions_per_tweet, words_per_tweet, words_per_reply, average_post_length, average_reply_length) VALUES ('"+username+"', "+total_statuses+", "+posts_count+", "+replies_count+", "+retweets_count+
+                ", "+listed_count+", "+favourites_count+", "+following_count+", "+followers_count+", "+description_length+", "+retweets_count+", "+favourite_count+", "+averageHashtagNumPerTweet+
+                ", "+averageUrlNumPerTweet+", "+averageMentionsPerTweet+", "+wordsPerTweet+", "+wordsPerReply+", "+averagePostLength+", "+averageReplyLength+")";
+
+                con.query(user_data_sql, function (err, result) {
+                    if (err) throw err;
+                    console.log("1 record inserted");
+                    con.end()
+                });
+        });
 }
 
 
 function getPageOfTweets(concat_tweets, iteration, maxIterations, access_token){
 
     //when all async requests are done - begin process to insert user's data to database
+    console.log(iteration+" "+maxIterations)
     if(iteration > maxIterations){
         insertDataToDatabase(concat_tweets)
     }
     else {
         var options = {
         hostname: 'api.twitter.com',
-        path: '/1.1/statuses/user_timeline.json?screen_name=notskend&count=200&tweet_mode=extended&include_rts=true&exclude_replies=false&page='+iteration,
+        path: '/1.1/statuses/user_timeline.json?screen_name='+username+'&count=200&tweet_mode=extended&include_rts=true&exclude_replies=false&page='+iteration,
         headers: {
             Authorization: 'Bearer ' + access_token
         }
@@ -244,16 +328,20 @@ function getPageOfTweets(concat_tweets, iteration, maxIterations, access_token){
             });
             result.on('end', function () {
             var tweets = JSON.parse(buffer);
-            console.log("async")
+            //console.log(tweets)
             concat_tweets = concat_tweets.concat(tweets)
             getPageOfTweets(concat_tweets, iteration+1, maxIterations, access_token)
             });
         });
     }
-    
+
+}
+
+function main(){
+        var userExists = checkAndHandleForExistingUser()
+        if(!userExists)
+                getTwitterInfo()
 }
 
 
-
-
-getTwitterInfo()
+//getTwitterInfo()
